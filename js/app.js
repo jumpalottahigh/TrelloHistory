c83a99757c64cdeaa16495da113ed05d81d605a1
Georgi Yanev <georgiyanev.gy@gmail.com>
2015-09-26 20:08:11 +0300

    Figuring our the API

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ Trello.authorize({
   name: "Getting Started Application",
   scope: {
     read: true,
-    write: true },
+    write: false },
   expiration: "never",
   authenticationSuccess,
   authenticationFailure
@@ -14,8 +14,18 @@ Trello.authorize({
 var authenticationSuccess = function() { console.log("Successful authentication"); };
 var authenticationFailure = function() { console.log("Failed authentication"); };
 
+
+//USED IDS:
+//MASS BOARD ID:
+//559e2b0a1b00224596d7e1e4
+
+
 $(document).ready(function(){
-  $('#fetch').on('click', function(){
+  //INSERT A CONTAINS METHOD IN THE STRING PROTOTYPE
+  String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
+
+  //FETCH BOARD BUTTON CLICKED
+  $('#fetch_board').on('click', function(){
     //USER IS AUTHENTICATED?
     if(authenticationSuccess) {
       //GRAB Board name
@@ -32,41 +42,104 @@ $(document).ready(function(){
           .addClass("alert-success");
 
         for (board of successMsg) {
-          console.log(board);
 
-          if(board.name === boardToGet) {
+          if(board.name == boardToGet) {
             //BINGO WE FOUND THAT BOARD
-            $('#status').append("<br>" + JSON.stringify(board));
+            $('#status').html(JSON.stringify(board))
+              .addClass("alert-success");
+            console.log("success");
           } else {
             $('#status').html("");
             $('#status').html("NO SUCH BOARD")
               .addClass("alert-danger");
           }
         }
+
       };
 
       //CALL THE BOARD FORTH
       Trello.get('/member/me/boards', success, error);
-      $('#statusObject').html(Trello);
-
-      $('#statusObject').html("ALL SUBOBJECTS" + JSON.stringify(Trello));
-
-
     }
   });
+
+  $('#fetch_list').on('click', function(){
+    if(authenticationSuccess) {
+      var success = function(successMsg) {
+        console.log(successMsg);
+
+        for (var i = 0; i < successMsg.length; i++) {
+          if(successMsg[i].name.contains("2015")){
+            console.log(successMsg[i].name);
+            console.log(successMsg[i]);
+          }
+        }
+      };
+
+      var error = function(errorMsg) {
+        console.log(errorMsg);
+      };
+
+      Trello.get('/boards/559e2b0a1b00224596d7e1e4/lists', success, error);
+    }
+  });
+
+  $('#get_all_boards').on('click', function(){
+    if(authenticationSuccess) {
+      Trello.get('members/me/boards/', {fields: "id,name,closed"}, function(boardsArray, err){
+        // if(err) console.log(err);
+        for (var i = 0; i < boardsArray.length; i++) {
+          console.log(boardsArray[i]);
+          $('#status').append(boardsArray[i].name + "<br>");
+        }
+
+      });
+    }
+  });
+
+  $('#get_all_lists').on('click', function(){
+    //massboard id
+    //559e2b0a1b00224596d7e1e4
+    if(authenticationSuccess) {
+      Trello.get('/boards/559e2b0a1b00224596d7e1e4/lists', {fields: "id,name,closed"}, function(success, error){
+        console.log(success);
+        for (var i = 0; i < success.length; i++) {
+          $('#status').append(success[i].name + "<br>");
+        }
+      });
+    }
+  });
+
+  $('#search').on('click', function(){
+    if(authenticationSuccess) {
+      //BabyLove BOARD ID
+      //559e675d35adcdb876f96958
+  var tok = localStorage.getItem("trello_token");
+  var key = "";
+
+  $("#status").html("");
+  // console.log(tok);
+  // console.log(key);
+
+      $.ajax({
+        url: "https://api.trello.com/1/search?query=is:archived&key="+key+"&token="+tok,
+        cache: false,
+        success: function(data){
+          console.log(data);
+          for (var i = 0; i < data.cards.length; i++) {
+            $("#status").append(data.cards[i].name + "<br>");
+          }
+        }
+      });
+    }
+  });
+
+  $('#get_all_cards').on('click', function(){
+    if(authenticationSuccess) {
+      Trello.get('/members/me/cards', function(success, error){
+        console.log(success);
+      });
+    }
+  });
+
+
 });
-
-//IF USER AUTHORIZED GET BOARDS
-
-// if(authenticationSuccess) {
-//
-//   var success = function(successMsg) {
-//     console.log(successMsg);
-//   };
-//
-//   var error = function(errorMsg) {
-//     console.log(errorMsg);
-//   };
-//
-//   Trello.get('/member/me/boards', success, error);
-// }
